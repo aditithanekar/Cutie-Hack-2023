cb76b6c261b5a097274eba4ad5cc47c95d3863bf
Keerthi <keerthi@Ashoks-MacBook-Pro-7.local>
2023-11-18 16:14:10 -0800

    here's what i changed

--- image_to_text.py
+++ image_to_text.py
@@ -1,28 +1,40 @@
-import pytesseract
-import os
 from PIL import Image
+import pytesseract
+import pyperclip
 
-# Set the path to the Tesseract executable (modify this path based on your installation)
-pytesseract.pytesseract.tesseract_cmd = '/usr/local/Cellar/tesseract/5.3.3/bin/tesseract'
+def image_to_text(image_path):
+    # Open the image file
+    img = Image.open(image_path)
 
+    # Use pytesseract to do OCR on the image
+    text = pytesseract.image_to_string(img)
 
-def convert():
-    #img = Image.open('/Users/keerthi/Desktop/hello.png')
-    #img = Image.open('/Users/keerthi/Desktop/cutie.jpg')
-    #img = Image.open('/Users/keerthi/Desktop/3 4.png')
-    #img = Image.open('/Users/keerthi/Desktop/3.png')
-    img = Image.open('/Users/keerthi/Desktop/11.png')
+    return text
 
-    #for single number:
-    #text = pytesseract.image_to_string(img, config='--psm 6')
+def text_to_latex(text):
+    # Basic formatting for LaTeX code
+    latex_code = f"\\documentclass{{article}}\n\\begin{{document}}\n{text}\n\\end{{document}}"
 
-    #for more numbers:
-    text = pytesseract.image_to_string(img, lang='eng+equ')
+    return latex_code
 
-    print(text)
-   # file1 = open("output.txt","a")
-   # file1.write(text)
-   # file1.write("\n")
-   # file1.close()
+def main():
+    # Ask the user for the path to the image file
+    image_path = input("Enter the path to the image file: ")
 
-convert()
+    # Extract text from the image
+    extracted_text = image_to_text(image_path)
+
+    # Convert extracted text to LaTeX code
+    latex_code = text_to_latex(extracted_text)
+
+    # Copy the LaTeX code to the clipboard
+    pyperclip.copy(latex_code)
+
+    print("LaTeX code has been copied to the clipboard.")
+    # DELETE THIS AFTER - TEMPORARY JUST FOR US TO SEE WHAT IS GETTING COPIED ONTO THE CLIPBOARD
+    print(latex_code)
+
+if __name__ == "__main__":
+    main()
+
+# use this to check: /Users/keerthi/Desktop/trydiv.png  or /Users/keerthi/Desktop/wp2.png
